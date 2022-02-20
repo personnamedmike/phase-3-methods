5efb351ba5ef9abb5578003a97a0745432cc2c85
Mike Starr <imhs@protonmail.com>
2022-02-20 13:43:51 -0800

    commit tree b80638809df43fbc0c9274471cd6e5dc45cfd67c

--- js/index.rb
+++ js/index.rb
@@ -1,4 +1,3 @@
-# Your code here!
 def greet_programmer
     puts "Hello, programmer!"
 end
@@ -8,24 +7,16 @@ def greet(name)
 end
 
 def greet_with_default(name="programmer")
-    puts "Hello, #{name}!"
+    puts "Hello, #{name}"
 end
 
 def add(num1, num2)
     num1 + num2
 end
 
-# greet_programmer
-
-# greet("mike")
-
-# greet_with_default("josh")
-
-# add(1,4)
-
 def halve number
-    if(typeof number != integer)
+    if(typeof number!="number")
         return nil
 
-    else number / 2
+    number / 2
     end
